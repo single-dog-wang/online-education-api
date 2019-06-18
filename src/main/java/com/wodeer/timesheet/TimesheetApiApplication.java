bb176f6886a04c76da6145b56cf914ae3df6974b
赵工 <4808844@qq.com>
2019-06-18 17:22:36 +0800

    首次代码检查

--- src/main/java/com/wodeer/timesheet/TimesheetApiApplication.java
+++ src/main/java/com/wodeer/timesheet/TimesheetApiApplication.java
@@ -1,8 +1,12 @@
-package com.wodeer.timesheetapi;
+package com.wodeer.timesheet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author richard
+ * Spring Boot 启动类
+ */
 @SpringBootApplication
 public class TimesheetApiApplication {
 
