2d63aa47e269baafb8020df6ba5801ae45ed1450
赵工 <4808844@qq.com>
2019-06-18 11:16:57 +0800

    init

---
+++ src/test/java/com/wodeer/timesheetapi/TimesheetApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wodeer.timesheetapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TimesheetApiApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
