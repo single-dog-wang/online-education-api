f2e5a89e90b7a53c1fa2d28893eaa72b71c50583
杨晓金 <983268995@qq.com>
2019-07-12 08:59:21 +0800

    Update NewPwdController.java

--- src/main/java/com/wodeer/timesheet/controller/NewPwdController.java
+++ src/main/java/com/wodeer/timesheet/controller/NewPwdController.java
@@ -4,7 +4,7 @@ import com.wodeer.timesheet.entity.User;
 import com.wodeer.timesheet.formobject.NewPwdFo;
 import com.wodeer.timesheet.model.ApiResult;
 import com.wodeer.timesheet.service.NewPwdService;
-import com.wodeer.timesheet.util.Md5Util;
+import com.wodeer.timesheet.util.MD5Util;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
